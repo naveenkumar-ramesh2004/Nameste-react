f0f4ee43155955df913c86aed3ad96ac04d9ec80
naveenkumar-ramesh2004 <naveenexpress461026@gmail.com>
2025-06-03 09:33:15 +0530

    feat: add search input with state handling for restaurants and food

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,17 @@ import Logo from '../assets/SoruVandi.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass, faUser, faPercent, faShoppingCart,faHeadset } from '@fortawesome/free-solid-svg-icons';
 import '../Style/Header.css';
+import React, { useState } from 'react';
 
 const Header = () => {
+    const [searchText, setSearchText] = useState("");
     return(
         <header className="header">
             <div className="Logo">
                 <img src={Logo} alt="Soru Vandi" />
             </div>
             <div className="searchbar">
-                <input type="text" placeholder="Search for Restarunts and Food"/>
+                <input type="text" placeholder="Search for Restarunts and Food" value={searchText} onChange={(e) => {setSearchText(e.target.value)}}/>
                 <button className="search-button"><FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon></button>
             </div>
             <nav>
